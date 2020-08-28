a467042ccf5684db04d2675c3c395e40abb60f7d
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2020-08-28 14:26:43 +0200

    add signup

---
+++ badoobot.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+import sys
+import os
+import random
+import string
+import pyautogui
+from time import sleep
+
+import variables
+
+class DabooBot():
+
+    def randomStr(N):
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+
+    phoneNumber = "0633108175"
+
+    driver = webdriver.Chrome(variables.driverpath)
+    sleep(1)
+    driver.get("https://badoo.com/signup/")
+    sleep(2)
+
+    driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/section/div/div/div[1]/form/div[1]/div[2]/div/input').send_keys(variables.name)
+    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[1]/div/div[1]').send_keys("1")
+    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[2]/div/div[1]').send_keys("J")
+    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[3]/div/div[1]').send_keys("1")
+    
+    driver.find_element_by_xpath('//*[@id="location_field"]').click()
+    driver.find_element_by_xpath('//*[@id="data-list-location-list"]/li[1]').click()
+
+    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[4]/div[2]/div/label[1]').click()
+
+    driver.find_element_by_xpath('//*[@id="login"]').send_keys(phoneNumber)
+    driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/section/div/div/div[1]/form/div[7]/div[2]/div/input').send_keys(randomStr(10))
+    sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[8]/button').click()
---
+++ variables.py
@@ -0,0 +1,2 @@
+driverpath = r"C:\Users\jordy\downloads\chromedriver_win32\chromedriver.exe"
+name = "bob"
