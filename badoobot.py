c2f00d24b943108edf54c81b7d5c46dc8778df6c
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2020-08-31 12:17:32 +0200

    fixed error

--- badoobot.py
+++ badoobot.py
@@ -14,68 +14,89 @@ from PIL import Image
 
 import variables
 
-class DabooBot():
+def randomStr(N):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
 
-    def randomStr(N):
-        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
 
-    def check_exists_by_xpath(xpath):
-        try:
-            driver.find_element_by_xpath(xpath)
-        except NoSuchElementException:
-            return False
-        return True
+def changeRandomPixel():
+    originalPic = variables.originalPic
+    im = Image.open(originalPic)
+    pic = Image.open(originalPic)
+    max_x, max_y = pic.size
+    pixels = im.load()
+    x = random.randrange(max_x)
+    y = random.randrange(max_y)
+    red = randint(1 , 256)
+    green = randint(1 , 256)
+    blue = randint(1 , 256) 
+    pixels[randint(1, max_x), randint(1, max_y)] = (red, green, blue, 1)
+    im.thumbnail(pic.size)
+    im.save(variables.newPic)
+    return variables.newPic
 
-    def changeRandomPixel():
-        originalPic = variables.originalPic
-        im = Image.open(originalPic)
-        pic = Image.open(originalPic)
-        max_x, max_y = pic.size
-        pixels = im.load()
-        x = random.randrange(max_x)
-        y = random.randrange(max_y)
-        red = randint(1 , 256)
-        green = randint(1 , 256)
-        blue = randint(1 , 256) 
-        pixels[randint(1, max_x), randint(1, max_y)] = (red, green, blue, 1)
-        im.thumbnail(pic.size)
-        im.save(variables.newPic)
-        return variables.newPic
-
-    driver = webdriver.Chrome(variables.driverpath)
-    sleep(1)
-    driver.get("https://badoo.com/signup/")
-    sleep(2)
-
-    driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/section/div/div/div[1]/form/div[1]/div[2]/div/input').send_keys(variables.name)
-    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[1]/div/div[1]').send_keys("1")
-    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[2]/div/div[1]').send_keys("J")
-    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[3]/div/div[1]').send_keys("1")
-    
-    driver.find_element_by_xpath('//*[@id="location_field"]').click()
-    driver.find_element_by_xpath('//*[@id="data-list-location-list"]/li[1]').click()
-
-    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[4]/div[2]/div/label[1]').click()
-
-    driver.find_element_by_xpath('//*[@id="login"]').send_keys(variables.phoneNumber)
-    driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/section/div/div/div[1]/form/div[7]/div[2]/div/input').send_keys(randomStr(10))
-    
-    sleep(2)
-    
-    driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[8]/button').click()
-
-    sleep(2)
-    
+def uploadPicture(driver):
     while True:
-            try:
-                driver.find_element_by_xpath('//*[@id="simple-page"]/div[2]/section/div[1]/div[2]/div[1]/div/div').click()
-                sleep(1)
-                pyautogui.write(variables.path + variables.newPic)
-                pyautogui.press('enter')
-                break
-            except:
-                continue
+        try:
+            sleep(1)
+            changeRandomPixel()
+            driver.find_element_by_xpath('//*[@id="simple-page"]/div[2]/section/div[1]/div[2]/div[1]/div/div').click()
+            sleep(1)
+            pyautogui.write(variables.path + variables.newPic)
+            pyautogui.press('enter')
+            return True
+        except:
+            print("failed to upload picture. retry")
+            continue
 
-    sleep(1)
+driver = webdriver.Chrome(variables.driverpath)
+sleep(1)
+driver.get("https://badoo.com/signup/")
+sleep(2)
 
-    driver.find_element_by_xpath('//*[@id="simple-page"]/div[2]/section/div[1]/div[4]/div').click()
+driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/section/div/div/div[1]/form/div[1]/div[2]/div/input').send_keys(variables.name)
+driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[1]/div/div[1]').send_keys("1")
+driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[2]/div/div[1]').send_keys("J")
+driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[2]/div[2]/div/div[3]/div/div[1]').send_keys("1")
+
+driver.find_element_by_xpath('//*[@id="location_field"]').click()
+driver.find_element_by_xpath('//*[@id="data-list-location-list"]/li[1]').click()
+
+driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[4]/div[2]/div/label[1]').click()
+
+driver.find_element_by_xpath('//*[@id="login"]').send_keys(variables.phoneNumber)
+driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/section/div/div/div[1]/form/div[7]/div[2]/div/input').send_keys(randomStr(10))
+
+sleep(2)
+
+driver.find_element_by_xpath('//*[@id="page"]/div[1]/div[3]/section/div/div/div[1]/form/div[8]/button').click()
+
+sleep(2)
+
+# foto
+uploadPicture(driver)
+
+sleep(1)
+
+driver.find_element_by_xpath('//*[@id="simple-page"]/div[2]/section/div[1]/div[4]/div').click()
+
+sleep(1)
+
+driver.find_element_by_xpath('/html/body/aside/section/div[1]/div/div/section/div/div[2]/div').click()
+
+# swipe
+while True:
+    try:
+        driver.find_element_by_xpath('//*[@id="mm_cc"]/div[1]/section/div/div[2]/div/div[2]/div[1]/div[1]').click()
+    except:
+        try:
+            driver.find_element_by_xpath('/html/body/aside/section/div[1]/div/div[3]/i').click()
+            sleep(0.5)
+        except:
+            print("couldn't find or click like button")
+            break
